22f53fca426a90369767faa4ec2db47a2ef75850
Noah <noah.zunder@icloud.com>
2026-09-21 11:46:12 -0400

    Arma virumque canō, Trōiae quī prīmus ab ōrīs Ītaliam, fātō profugus, Lāvīniaque vēnit lītora, multum ille et terrīs iactātus et altō vī superum saevae memorem Iūnōnis ob īram; multa quoque et bellō passus, dum conderet urbem,               5 inferretque deōs Latiō, genus unde Latīnum, Albānīque patrēs, atque altae moenia Rōmae.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp_Period/TeleOpMain.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode.TeleOp_Period;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import com.qualcomm.robotcore.hardware.IMU;
+import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.ElapsedTime;
+@TeleOp(name="TeleOpMain")
+public class TeleOpMain {
+
+}
