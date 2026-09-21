24e8b266dc6f309f80fec938b9dae28da63c68c6
Noah <noah.zunder@icloud.com>
2026-09-21 12:05:10 -0400

    Arma virumque canō, Trōiae quī prīmus ab ōrīs Ītaliam, fātō profugus, Lāvīniaque vēnit lītora, multum ille et terrīs iactātus et altō vī superum saevae memorem Iūnōnis ob īram; multa quoque et bellō passus, dum conderet urbem,               5 inferretque deōs Latiō, genus unde Latīnum, Albānīque patrēs, atque altae moenia Rōmae.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp_Period/TeleOpMain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp_Period/TeleOpMain.java
@@ -1,9 +1,11 @@
 package org.firstinspires.ftc.teamcode.TeleOp_Period;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+
+import com.acmerobotics.roadrunner.Pose2d;
 import com.qualcomm.robotcore.hardware.IMU;
 import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
-
+import org.firstinspires.ftc.teamcode.MecanumDrive;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
@@ -11,8 +13,16 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
 @TeleOp(name="TeleOpMain")
 public class TeleOpMain extends LinearOpMode{
+    private MecanumDrive drive;
+    ElapsedTime runtime = new ElapsedTime();
+
     @Override
     public void runOpMode() throws InterruptedException {
-
+        drive = new MecanumDrive(hardwareMap,new Pose2d(0, 0, 0));
+        waitForStart();
+        runtime.reset();
+        while(opModeIsActive()){
+            drive.lazyImu();
+        }
     }
 }
