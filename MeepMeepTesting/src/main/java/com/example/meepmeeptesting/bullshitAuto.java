fef2129ad108c6f5c921583b04d02aa3d224bece
potatos2g <potatos2e@gmail.com>
2026-09-21 09:21:00 -0400

    got rid of all the unnecessary turning during the movement, secret chinese auto added

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/bullshitAuto.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/bullshitAuto.java
@@ -10,26 +10,22 @@ import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
-public class launchPark {
+public class bullshitAuto {
     public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(700);
 
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 18)
+                .setConstraints(1, 0.01, Math.toRadians(1800000), Math.toRadians(5*1800), 18)
                 .build();
 
-        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(61, -12, Math.toRadians(180)))
-                .strafeTo(new Vector2d(40,-12))
-                //launch()
-                .waitSeconds(2)
-                .setTangent(Math.toRadians(-90))
-                //.turn(Math.toRadians(90))
-                .splineToSplineHeading(new Pose2d(0,-45,Math.toRadians(90)),Math.toRadians(180))
-                .setTangent(Math.toRadians(180))
-                .splineToConstantHeading(new Vector2d(-24,-55),Math.toRadians(180))
+        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(-61, -9, Math.toRadians(0)))
+                .splineToLinearHeading(new Pose2d(-50,-55,Math.toRadians(90)),Math.toRadians(-90))
 
+                .turn(Math.toRadians(360000))
+                .strafeTo(new Vector2d(100,-55))
                 .build());
+
         Image customImg = null;
         try {
             customImg = ImageIO.read(new File("FIELD_BIOBUZZ_OFFICAL.png"));
